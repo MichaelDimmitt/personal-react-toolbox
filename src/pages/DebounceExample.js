4ddcf88054c75884eb624e67617fc3d1aa1a80d9
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-01-27 20:07:24 -0500

    simplify debounce example

--- src/pages/DebounceExample.js
+++ src/pages/DebounceExample.js
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Home = () => {
+const DebounceExample = () => {
   return (
     <div>
-      <h2>
-        Home Page, will list the paths of all standard sections of this react application.
-      </h2>
-      <a></a>debounce
+      debounce example
     </div>
   );
 };
 
-export default Home;
+export default DebounceExample;
