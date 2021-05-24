c7b7507b9a66ddb61ab4f84b306403b55f3e7a69
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-05-24 08:59:08 -0400

    try different things to get hooks working

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,13 @@
-import React, { Suspense, lazy } from "react";
+import * as React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CustomErrorBoundary from "./pages/ErrorPage";
 import LoadingPage from "./pages/LoadingPage";
 import Home from "./pages/Home/src/index.js"; // priority load
 import { DEBOUNCE, HOME } from "./constants/routes";
+const {Suspense} = React
 
 // If you want a load screen. START HERE
-const DebounceExample = lazy(() => import("./pages/DebounceExample"));
+const DebounceExample = React.lazy(() => import("./pages/DebounceExample"));
 
 function Router({ state, dispatch }) {
   return (
